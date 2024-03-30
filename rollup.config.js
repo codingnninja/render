ffb0c90513a2ef287eb4bb83c3961fd5fc225970
ayobami_ogundiran <ayobami_ogundiran@yahoo.com>
2024-03-31 00:49:22 +0100

    remove babel, node resolve and commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
 import pkg from './package.json' assert {type:"json"};
 
 export default [
@@ -11,13 +8,7 @@ export default [
       { name: 'render', file: pkg.module, format: 'es' },
     ],
     plugins: [
-      terser(),
-      nodeResolve({
-        browser: true
-      }),
-      babel({
-        exclude: 'node_modules/**',
-      }),
+      terser()
     ],
   },
 ];
